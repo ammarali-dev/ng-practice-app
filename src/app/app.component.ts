9d6192d0c3df7b6cd8a58ef2ce177a88da086c16
ammarali-dev <tabraiz.ammarali@gmail.com>
2024-03-04 12:35:38 +0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ng-assignment-app';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber);
+    if (firedNumber % 2 == 0) this.evenNumbers.push(firedNumber);
+    else this.oddNumbers.push(firedNumber);
+  }
 }
